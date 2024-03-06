e37e701f83befd4dad6a222b560e49083e033875
Deboneil <bhattacharjeedeboneil@gmail.com>
2024-03-06 23:11:40 +0530

    ✨ New Feature: dark mode

--- PocketPad.py
+++ PocketPad.py
@@ -13,7 +13,6 @@ try:
 except tk.TclError:
     print("Warning: Icon file 'favicon.ico' not found or could not be loaded.")
 
-
 def open_file():
     filepath = askopenfilename(
         filetypes=[('Text Files', "*.txt"), ("All Files", "*.*")]
@@ -37,17 +36,32 @@ def save_file():
         output_files.write(text)
     window.title(f"Text Editor - {filepath}")    
 
+def dark_mode():
+    status = btn_dark.cget("text")
+    if (status == "Dark Mode"):
+        txt_edit.config(bg="black", fg="#16FF00")
+        frm_buttons.config(bg="black", cursor="cross")
+        btn_open.config(bg="black", fg="#16FF00")
+        btn_save.config(bg="black", fg="#16FF00")
+        btn_dark.config(bg="black", fg="#16FF00", text="Light Mode")
+    if (status == "Light Mode"):
+        txt_edit.config(bg="white", fg="black")
+        frm_buttons.config(bg="white", cursor="arrow")
+        btn_open.config(bg="white", fg="black")
+        btn_save.config(bg="white", fg="black")
+        btn_dark.config(bg="white", fg="black", text="Dark Mode")
 
-txt_edit = tk.Text(master=window)
+txt_edit = tk.Text(master=window, font=("Sans, 14"))
 frm_buttons = tk.Frame(master=window, relief=tk.RAISED, border=2)
 btn_open = tk.Button(master=frm_buttons, text="Open", command=open_file)
 btn_save = tk.Button(master=frm_buttons, text="Save As", command=save_file)
+btn_dark = tk.Button(master=frm_buttons, text="Dark Mode", command=dark_mode)
 
 btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
 btn_save.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
+btn_dark.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
 
 frm_buttons.grid(row=0, column=0, sticky="ns")
 txt_edit.grid(row=0, column=1, sticky="nsew")
-
 
 window.mainloop()
